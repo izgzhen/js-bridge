b9ff2a1679c9593fd8632bb35bf23aa417577dd8
Zhen Zhang <izgzhen@gmail.com>
2016-08-14 16:08:14 +0200

    add case 3

--- Test/Client.hs
+++ Test/Client.hs
@@ -2,28 +2,40 @@ import Language.JS.Type
 import Language.JS.Platform
 import Control.JS.Utils.Client
 
-domains :: Domains
-domains =
-    let x    = JEVar  (Name "x")
-        zero = JEPrim (PNumber 0.0)
-    in Domains [(PTyNumber, [ "x" .@ (x .> zero)
-                            , "x" .@ (x .== zero)
-                            , "x" .@ (x .< zero)])]
+x    = JEVar  (Name "x")
+zero = JEPrim (PNumber 0.0)
 
-caseSimpleEval :: IO ()
-caseSimpleEval = do
+domains :: Domains
+domains = Domains [(PTyNumber, [ "x" .@ (x .> zero)
+                               , "x" .@ (x .== zero)
+                               , "x" .@ (x .< zero)])]
+
+
+onePointNine :: JsUnionVal
+onePointNine = JsUnionVal [JVPrim PTyNumber ("x" .@ (x .== JEPrim (PNumber 1.9)))]
+
+caseObjEval :: IO ()
+caseObjEval = do
     idl <- readFile "Test/prelude.webidl"
     startSession domains idl $ \handler -> do
         res <- newCons handler (Name "Bar") []
         print res
 
-caseWrongNumArgs :: IO ()
-caseWrongNumArgs = do
+casePrimEval :: IO ()
+casePrimEval = do
     idl <- readFile "Test/prelude.webidl"
     startSession domains idl $ \handler -> do
         res <- call handler (LInterface (Name "Foo")) (Name "pos") []
         print res
 
+caseWrongArgNum :: IO ()
+caseWrongArgNum = do
+    idl <- readFile "Test/prelude.webidl"
+    startSession domains idl $ \handler -> do
+        res <- call handler (LInterface (Name "Foo")) (Name "pos") [onePointNine]
+        print res
+
+
 main :: IO ()
-main = caseWrongNumArgs
+main = caseWrongArgNum
 
