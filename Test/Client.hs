0cb470e9c0567dd672664a6dd85b4afff8fd66bb
Zhen Zhang <izgzhen@gmail.com>
2016-08-14 17:34:31 +0200

    add case 4

--- Test/Client.hs
+++ Test/Client.hs
@@ -4,38 +4,45 @@ import Control.JS.Utils.Client
 
 x    = JEVar  (Name "x")
 zero = JEPrim (PNumber 0.0)
+zeroi = JEPrim (PInt 0)
+
 
 domains :: Domains
 domains = Domains [(PTyNumber, [ "x" .@ (x .> zero)
                                , "x" .@ (x .== zero)
-                               , "x" .@ (x .< zero)])]
-
+                               , "x" .@ (x .< zero)]),
+                   (PTyInt   , [ "x" .@ (x .> zeroi)
+                               , "x" .@ (x .== zeroi)
+                               , "x" .@ (x .< zeroi)])]
 
 onePointNine :: JsUnionVal
 onePointNine = JsUnionVal [JVPrim PTyNumber ("x" .@ (x .== JEPrim (PNumber 1.9)))]
 
-caseObjEval :: IO ()
-caseObjEval = do
+oneInt = JsUnionVal [JVPrim PTyInt ("x" .@ (x .== JEPrim (PInt 1)))]
+
+testHarness f = do
     idl <- readFile "Test/prelude.webidl"
-    startSession domains idl $ \handler -> do
-        res <- newCons handler (Name "Bar") []
-        print res
+    startSession domains idl f
+
+caseObjEval :: IO ()
+caseObjEval = testHarness $ \handler -> do
+    res <- newCons handler (Name "Bar") []
+    print res
 
 casePrimEval :: IO ()
-casePrimEval = do
-    idl <- readFile "Test/prelude.webidl"
-    startSession domains idl $ \handler -> do
-        res <- call handler (LInterface (Name "Foo")) (Name "pos") []
-        print res
+casePrimEval = testHarness $ \handler -> do
+    res <- call handler (LInterface (Name "Foo")) (Name "pos") []
+    print res
 
 caseWrongArgNum :: IO ()
-caseWrongArgNum = do
-    idl <- readFile "Test/prelude.webidl"
-    startSession domains idl $ \handler -> do
-        res <- call handler (LInterface (Name "Foo")) (Name "pos") [onePointNine]
-        print res
+caseWrongArgNum = testHarness $ \handler -> do
+    res <- call handler (LInterface (Name "Foo")) (Name "pos") [onePointNine]
+    print res
 
+caseCallbackEval :: IO ()
+caseCallbackEval = testHarness $ \handler -> do
+    res <- call handler (LInterface (Name "Foo")) (Name "async") [oneInt, JsUnionVal [JVClos 1]]
+    print res
 
 main :: IO ()
-main = caseWrongArgNum
-
+main = caseCallbackEval
